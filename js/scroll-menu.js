cd3b07d69ade3ab3785fc09f52be45b4747933a6
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-11-29 16:03:51 +0100

    listo hasta min-width: 400px

--- js/scroll-menu.js
+++ js/scroll-menu.js
@@ -4,15 +4,15 @@ window.addEventListener('scroll', function() {
     let positionHeaderStop = animateHeaderStop.getBoundingClientRect().top;
     let animateHeaderNav = document.querySelector(".header_bottom_container")
     let animateListNav = document.querySelector(".list_nav_container");
-    
 
-    if(positionHeaderStop <= -32) {
+
+    if(positionHeaderStop <= -44) {
         animateHeaderNav.classList.add("fixed_menu");
         animateListNav.classList.add("fixed_nav");
 
     }
 
-    if(positionHeaderStop > -32) {
+    if(positionHeaderStop > -44) {
         animateHeaderNav.classList.remove("fixed_menu");
         animateListNav.classList.remove("fixed_nav")
 
