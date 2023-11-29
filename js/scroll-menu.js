cbbf27852085b2b41612afa9be36be9376f2ca23
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-11-29 01:22:59 +0100

    articulo principar y footer lis para movil small

--- js/scroll-menu.js
+++ js/scroll-menu.js
@@ -3,9 +3,8 @@ window.addEventListener('scroll', function() {
     let animateHeaderStop = document.getElementById('headerStop');
     let positionHeaderStop = animateHeaderStop.getBoundingClientRect().top;
     let animateHeaderNav = document.querySelector(".header_bottom_container")
-    let animateListNav = document.querySelector(".list_nav_container")
+    let animateListNav = document.querySelector(".list_nav_container");
     
-    console.log(positionHeaderStop);
 
     if(positionHeaderStop <= -32) {
         animateHeaderNav.classList.add("fixed_menu");
